a44d5d9fcca33547f253a49bb06d216000f399f5
two-bits <tbjr@bongohut.com>
2026-08-01 17:59:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -117,7 +117,7 @@ def auth_status():
         }), 401
 
 
-@app.route("/list_vehicles", methods=["POST", "GET"])
+@app.route("/list_vehicles", methods=["GET"])
 def list_vehicles():
     if not authorize_request():
         return jsonify({"error": "Unauthorized"}), 403
